04995827a4ff2d2c0f359396e76e175b125c3c5a
jarangit <jaran.dch@gmail.com>
2022-06-12 19:46:21 +0700

    feat: complete create form for create the campaign

--- src/component/form/createCampaignForm.js
+++ src/component/form/createCampaignForm.js
@@ -7,9 +7,10 @@ const defaultValues = {
   typeCampaign: '',
   imageCampaign: '',
   descriptionCampaign: '',
-  time: '',
   goal: '',
   imageCampaign2: '',
+  startDate: '',
+  endDate: '',
 }
 
 const CreateCampaignForm = () => {
@@ -99,6 +100,90 @@ const CreateCampaignForm = () => {
                 onChange={onChange}
                 type={"text"}
                 className="bg-$gray-light w-full h-[96px] rounded-lg mt-2 pl-3" />
+            )}
+          />
+        </div>
+      </div>
+
+      <div id="box2" className="flex flex-col gap-5 py-5 px-3 border border-$gray-light rounded-lg">
+        <div>
+          <div className='text-sm font-semibold'>
+            ระยะเวลาในการดำเนินโครงการ
+          </div>
+          <div className="flex w-full gap-5 mt-3">
+            <div className="w-full">
+              <div className='text-sm'>
+                วันที่เริ่มต้น
+              </div>
+              <Controller
+                name="startDate"
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <input
+                    value={value}
+                    onChange={onChange}
+                    type={"date"}
+                    className="bg-$gray-light w-full h-[36px] rounded-lg mt-2 pl-3" />
+                )}
+              />
+            </div>
+            <div className="w-full">
+              <div className='text-sm'>
+                วันที่สิ้นสุด
+              </div>
+              <Controller
+                name="endDate"
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <input
+                    value={value}
+                    onChange={onChange}
+                    type={"date"}
+                    className="bg-$gray-light w-full h-[36px] rounded-lg mt-2 pl-3" />
+                )}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="box3" className="flex flex-col gap-5 py-5 px-3 border border-$gray-light rounded-lg">
+        <div>
+          <div className='text-sm font-semibold'>
+            เป้าหมาย
+          </div>
+          <Controller
+            name="goal"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <label className='relative'>
+                <input
+                  value={value}
+                  onChange={onChange}
+                  type={"text"}
+                  className="bg-$gray-light w-full h-[36px] rounded-lg mt-2 pl-3" />
+                <div className='absolute top-0 right-4'>บาท</div>
+              </label>
+            )}
+          />
+        </div>
+      </div>
+
+      <div id="box4" className="flex flex-col gap-5 py-5 px-3 border border-$gray-light rounded-lg">
+        <div >
+          <div className='text-sm font-semibold'>
+            อัพโหลดรูปแคมเปญ
+          </div>
+          <Controller
+            name={"imageCampaign2"}
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <div className='image_upload bg-$gray-light h-[56px] w-[56px] flex justify-center items-center rounded-lg mt-2 border-dashed border border-$gray'>
+                <label htmlFor="file-input2">
+                  <img src="/img/icon/uploadImage.png" alt="" width={"24px"} />
+                </label>
+                <input id="file-input2" type={"file"} onChange={onChange} value={value} />
+              </div>
             )}
           />
         </div>
